6727cae605656658ecbe426ac2b2f1d530369eaa
Zahraa <zzmmk2003@gmail.com>
2024-06-01 13:01:27 -0400

    session setup

--- src/pages/Chats/index.jsx
+++ src/pages/Chats/index.jsx
@@ -1,9 +1,15 @@
 import React, {useEffect,useState} from "react";
-import { Button, Grid } from "@mui/material";
-import Textarea from "@mui/material";
-import OpenAI from "openai";
 import { useParams } from "react-router-dom";
+import api from "../../api/sessions";
+import ChatStyles from "../../styles/chat";
+import Images from "../../constants/images";
+import RexMessage from "../../components/RexMessage";
+import UserMessage from "../../components/UserMessage";
+import OpenAI from "openai";
+import { Button, Grid } from "@mui/material";
+import Textarea from "@mui/base/TextareaAutosize";
 import useMediaQuery from "@mui/material";
+
 
 
 
@@ -19,6 +25,76 @@ const Chat = () => {
     const matches = useMediaQuery("(max-width: 600px)");
     let chatKeys = [];
 }
+
+useEffect(() => {
+    const fetchSession = async () => {
+        try {
+            const response = await api.get("/sessions");
+            setSessions(response.data);
+            setThisSession(
+                response.data.find(
+                    (session) => parseInt(session?.id, 10) === parseInt(id, 10)
+                )
+            );
+            handleScroll();
+            window.addEventListener("scroll", handleScroll);
+        }
+        catch(error){
+            if (error.response){
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            }
+            else{
+                console.log(error);
+            }
+        }
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        }
+    }
+    fetchSession();
+}, []);
+
+const handleScroll = () => {
+    const ScrollPos = window.scrollY;
+};
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    let updatedSession = {};
+
+    setTimeout(async function () {
+        const date = new Date();
+        const month = date.getMonth();
+        const year = date.getFullYear();
+        const formattedDate = months[month] + " " + day + " " + year;
+        callOpenAIAPI();
+        thisSession.chats.push({user: userPrompt, ReX: RexReply});
+        updatedSession = {
+            id: id,
+            date, formattedDate,
+            chats: thisSession.chats,
+            isSessionEnded: thisSession.isSessionEnded,
+        };
+        for (let i = 0; i < updatedSession.chats.length; i++) {
+            chatKeys.push(Object.keys(updatedSession.chats[i]));
+        }
+
+        try{
+            await api.patch(`/sessions/${id}`, updatedSession);
+            setSessions(
+                sessions.map((session) =>
+                    session.id === id ? updatedSession : session
+                )
+            );
+            setUserPrompt("");
+        }
+        catch(err){
+            console.log(`error: ${err.message}`);
+        }
+
+    })};
 
 
 export default App;
